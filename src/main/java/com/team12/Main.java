71c58037860d41803df08d528c0ad7714d3b48c7
hogiljung <horlf29@gmail.com>
2026-03-20 23:48:28 +0900

    Add List
    # Conflicts:
    #	src/main/java/com/team12/Main.java

--- src/main/java/com/team12/Main.java
+++ src/main/java/com/team12/Main.java
@@ -3,8 +3,12 @@ package com.team12;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     static void main() {
         Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
     }
 }
