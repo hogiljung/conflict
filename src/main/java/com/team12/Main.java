7ab4158b12f05ece0a714290fef93fdac2fa3df3
hogiljung <horlf29@gmail.com>
2026-03-20 22:11:43 +0900

    Add List

--- src/main/java/com/team12/Main.java
+++ src/main/java/com/team12/Main.java
@@ -1,7 +1,10 @@
 package com.team12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     static void main() {
-
+        List<Integer> list = new ArrayList<>();
     }
 }
