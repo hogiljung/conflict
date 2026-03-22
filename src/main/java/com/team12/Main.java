12dd55eecd0c53bdc60c38c766bd89a4fc141fcf
hogiljung <horlf29@gmail.com>
2026-03-22 19:31:45 +0900

    Add Map and List

--- src/main/java/com/team12/Main.java
+++ src/main/java/com/team12/Main.java
@@ -1,7 +1,13 @@
 package com.team12;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     static void main() {
-
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
     }
 }
